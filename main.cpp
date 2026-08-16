1619be188026edd87d567f6c498c4a63a99049fb
ThematicallyUnpossible <fernando_linux@proton.me>
2026-08-16 14:28:11 +0700

    utilized command arguments

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include "io_helper.h"
 #include "ptrace_module.h"
 
-int main(int, char**){
-    std::optional<PtraceModule::Object> test = PtraceModule::Object::instantiate("Discord");
+int main(int argc, char* argv[]){
+
+    if(argc != 2)
+    {
+        std::cerr << "Invalid usage. Expected : Tinker <program_name>" << "\n";
+        return 1;
+    }
+
+    std::optional<PtraceModule::Object> test = PtraceModule::Object::instantiate(argv[1]);
     std::cout << test->peek_data().m_string_target_pid;
     return 0;  
 }
