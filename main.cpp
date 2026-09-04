3a697cef93241bdf6ed2abb93b0afe03824b82f6
ThematicallyUnpossible <fernando_linux@proton.me>
2026-09-04 11:31:52 +0700

    choices inside run_inject

--- main.cpp
+++ main.cpp
@@ -15,13 +15,44 @@ void run_injector(char* argv)
     std::cout << "Program Entry : "<< PtraceModuleObject->peek_data().m_string_target_base << "\n";
     std::cout << "Program Libc Base  : "<< PtraceModuleObject->peek_data().m_string_target_libc_base << "\n";
 
-    if(!PtraceModuleObject->queue_loadable())
+    std::string table_of_choices
     {
-        std::cout << "Failed to queue library." << "\n";
+        "1. Create loadable \n"
+        "2. Load existing \n"
+        "3. Trigger hook function\n"
+        "4. Exit\n"
+        "Enter your choice : "
+    };
+    int choice{3};
+
+    while(true)
+    {
+        choice = get_input<int>(table_of_choices, 1, 4);
+        if(choice == 1)
+        {
+            clear_input();
+            if(!PtraceModuleObject->queue_loadable())
+            {
+                std::cout << "Failed to queue library." << "\n";
+                continue;
+            }
+        }
+        else if(choice == 2)
+        {
+            bool inject_result = PtraceModuleObject->inject_loadable();
+        }
+        else if (choice == 3)
+        {
+            trigger_hook_fcn(PtraceModuleObject->peek_data().m_string_target_pid);
+        }
+        else if (choice == 4)
+        {
+            return;
+        }
     }
 
-    bool inject_result = PtraceModuleObject->inject_loadable();
-    trigger_hook_fcn(PtraceModuleObject->peek_data().m_string_target_pid);
+    
+
 
 }
 
