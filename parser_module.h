0738f6f9fecb48a3948fa882feec1cbc04da71c2
ThematicallyUnpossible <fernando_linux@proton.me>
2026-09-12 21:56:53 +0700

    rewritted parser

--- parser_module.h
+++ parser_module.h
@@ -12,11 +12,13 @@
 
 inline void parse_elf64()
 {
-    std::string test = get_input<std::string>("Enter elf program path : ");
-    int fd = open(test.c_str(), O_RDONLY);
+    std::string path_to_program{get_input<std::string>("Enter program path : ")};
+
+
+    int fd = open(path_to_program.c_str(), O_RDONLY);
     if(fd < 0)
     {
-        std::cerr << "UNABLE TO OPEN FILE!" << "\n";
+        std::cerr << "UNABLE TO OPEN HANDLE TO PROGRAM : " << path_to_program << "\n";
         return;
     }
 
@@ -24,26 +26,33 @@ inline void parse_elf64()
     fstat(fd, &st);
 
     void* map = mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    close(fd);
-
     if(map == MAP_FAILED)
     {
-        std::cerr << "UNABLE TO MAP!" << "\n";
-    }
-
-    auto* header = static_cast<const Elf64_Ehdr*>(map);
-
-    if (header->e_ident[EI_MAG0] != ELFMAG0 ||
-        header->e_ident[EI_MAG1] != ELFMAG1 ||
-        header->e_ident[EI_MAG2] != ELFMAG2 ||
-        header->e_ident[EI_MAG3] != ELFMAG3) 
-    {
-        std::cerr << "NOT A VALID ELF!\n";
-        munmap(map, st.st_size);
+        std::cerr << "UNABLE TO MAP A REGION " << "\n";
         return;
     }
 
-    std::cout << "VALID ELF FILE!" << "\n";
+    auto* header = static_cast<Elf64_Ehdr*>(map);
+
+    if(
+        header->e_ident[EI_MAG0] != ELFMAG0 ||
+        header->e_ident[EI_MAG1] != ELFMAG1 ||
+        header->e_ident[EI_MAG2] != ELFMAG2 ||
+        header->e_ident[EI_MAG3] != ELFMAG3
+    )
+    {
+        munmap(map, st.st_size);
+        std::cout << "NOT A VALID ELF64 PROGRAM" << "\n";
+        return;
+    }
+
+    std::cout << "VALID ELF64!" << "\n";
+
+
+
+
+
+
 }
 
 
