6eb219e818b25ea226635fffa89e27bfb68aab93
ThematicallyUnpossible <fernando_linux@proton.me>
2026-09-18 09:40:31 +0700

    rewritten parser

--- parser_module.h
+++ parser_module.h
@@ -50,19 +50,13 @@ inline void parse_elf64()
 
     auto* section_header = reinterpret_cast<Elf64_Shdr*>(static_cast<char*>(map) + header->e_shoff);
 
-    const auto& shstrtab_hdr = section_header[header->e_shstrndx];
-
-    const char* shstrtab = static_cast<const char*>(map) + shstrtab_hdr.sh_offset;
-
-    std::cout << "--- ELF Sections ---\n";
-    for (int i = 0; i < header->e_shnum; ++i) 
+    const auto& shstrtab = section_header[header->e_shstrndx];
+    const char* shstrtab_hdr = static_cast<char*>(map) + shstrtab.sh_offset;
+    
+    for(int i{0}; i < header->e_shnum; i++)
     {
-        const char* name = &shstrtab[section_header[i].sh_name];
-        std::cout << "Name: " << name
-                  << " | Type: " << section_header[i].sh_type 
-                  << " | Size: " << section_header[i].sh_size << "\n";
+        std::cout << shstrtab_hdr + section_header[i].sh_name << "\n";
     }
-
 
 
 
